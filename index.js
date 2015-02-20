baf7f63a6236d79d9043f0b50774e3bb90a1be42
Christian Hedin <christian.hedin@jayway.com>
2015-02-20 15:01:42 +0100

    Uses PORT env var if available.

--- index.js
+++ index.js
@@ -394,7 +394,8 @@ io.on('connection', function(socket){
 
 // HTTP server:
 //
-http.listen(3000, function(){
+var port = (process.env.PORT || 3000)
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
 
